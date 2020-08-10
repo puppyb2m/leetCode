91690fccb5b5b85a72ecb88be205a11073aee308
Chris Zhang <kanginzz@gmail.com>
2020-08-10 23:35:45 +0900

    Decode Ways

--- dp/CanIWin.swift
+++ dp/CanIWin.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class Solution {
+    func canIWin(_ maxChoosableInteger: Int, _ desiredTotal: Int) -> Bool {
+        if maxChoosableInteger >= desiredTotal {
+            return true
+        }
+        if (maxChoosableInteger + 1) * maxChoosableInteger / 2 < desiredTotal {
+            return false
+        }
+        
+        var memo = [Int: Bool]()
+        return helper(maxChoosableInteger, desiredTotal, 0, &memo)
+    }
+    
+    fileprivate func helper(_ maxChoosableInteger: Int, _ desiredTotal: Int, _ used: Int, _ memo: inout [Int: Bool]) -> Bool {
+        if let canWin = memo[used] {
+            return canWin
+        }
+        
+        for i in 0..<maxChoosableInteger {
+            let current = 1 << i
+            if current & used == 0 {
+                if desiredTotal <= i + 1 || !helper(maxChoosableInteger, desiredTotal - i - 1, current | used, &memo) {
+                    memo[used] = true
+                    return true
+                }
+            }
+            
+        }
+        
+        memo[used] = false
+        return false
+    }
+}
---
+++ dp/DecodeWays.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shun Zhang on 2020/08/10.
+//
+
+import Foundation
+
