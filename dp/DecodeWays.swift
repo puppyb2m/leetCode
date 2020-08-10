ad2a73d1966372bdfb297dc3c74a088c002be2e6
Chris Zhang <kanginzz@gmail.com>
2020-08-10 23:35:58 +0900

    no message

--- dp/DecodeWays.swift
+++ dp/DecodeWays.swift
@@ -7,3 +7,30 @@
 
 import Foundation
 
+class Solution {
+    func numDecodings(_ s: String) -> Int {
+        let s = Array(s)
+        var dp = Array(repeating: 0, count: s.count + 1)
+        dp[0] = 1
+        
+        for i in 1...s.count {
+            if s[i - 1] != "0" {
+                dp[i] += dp[i - 1]
+            }
+            
+            if i > 1 && isValid(s, i - 2, i - 1) {
+                dp[i] += dp[i - 2]
+            }
+        }
+        
+        return dp[s.count]
+    }
+    
+    private func isValid(_ s: [Character], _ start: Int, _ end: Int) -> Bool {
+        guard let num = Int(String(s[start...end])) else {
+            fatalError()
+        }
+        
+        return num >= 10 && num <= 26
+    }
+}
