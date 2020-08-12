c0a77ded8340b4067fcb745c42770832d2631b40
Chris Zhang <kanginzz@gmail.com>
2020-08-12 23:08:14 +0900

    no message

--- dp/GenerateParentheses.swift
+++ dp/GenerateParentheses.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class GenerateParentheses {
+    func generateParenthesis(_ n: Int) -> [String] {
+        guard n > 0 else {
+            return [String]()
+        }
+        var paths = [String](), path = ""
+        dfs(&paths, path, n, n)
+        return paths
+    }
+    private func dfs(_ paths: inout [String], _ path: String, _ leftRemaining: Int, _ rightRemaining: Int) {
+        if rightRemaining == 0 {
+            paths.append(path)
+            return
+        }
+        if leftRemaining > 0 {
+            dfs(&paths, path + "(", leftRemaining - 1, rightRemaining)
+        }
+        if rightRemaining > leftRemaining {
+            dfs(&paths, path + ")", leftRemaining, rightRemaining - 1)
+        }
+    }
+}
