d02971b1b7576e91dc8dfc64dc4902167d7fcef9
Chris Zhang <kanginzz@gmail.com>
2021-03-17 09:55:42 +0900

    162. Find Peak Element

--- Binary Search/162. Find Peak Element.swift
+++ Binary Search/162. Find Peak Element.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+// 1 https://leetcode.com/problems/find-peak-element/ 162. Find Peak Element
+// 2 source code
+class Solution {
+    func findPeakElement(_ nums: [Int]) -> Int {
+        var left = 0, right = nums.count - 1 , mid = 0
+        while left < right{
+            mid = left + (right - left)/2
+            if nums[mid] > nums[mid+1]{
+                right = mid
+            }else{
+                left = mid + 1
+            }
+        }
+        
+        return left
+    }
+}
